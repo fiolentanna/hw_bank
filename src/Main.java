d3223e00116488079c057e92e26ab10a9d155d81
fiolentanna <tigranka@gmail.com>
2021-04-01 15:24:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        int current_balance = 2_000_000_000;
+        int transfer = 500_000_000;
+        int total = current_balance + transfer;
+        System.out.println(total);
+    }
+}
